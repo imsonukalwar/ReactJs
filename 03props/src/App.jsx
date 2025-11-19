36cbed441bcf5b68c67c106305b9ea5f0da3af73
unknown <imsonukalwar@gmail.com>
2025-11-19 16:41:28 +0530

    update

---
+++ 03props/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Product from './product'
+
+function App() {
+  // const name="laptop"
+  // const name1="dipu";
+
+  return (
+    <>
+    <Product g= "sonu" a="20"/>
+    <Product g= "dipu" a={24}/>
+    </>
+  )
+}
+
+export default App
---
+++ 03props/src/product.jsx
@@ -0,0 +1,20 @@
+import "./product.css"
+
+// function Product({name,info}){
+//     return(
+//     <div className="Product">
+//     <h1>product Title {name} {info.name} {info.age} {info.gender}</h1>
+//     <h3>product description</h3>
+//     </div>
+//     )
+// }
+function Product({g,a}){
+    return(
+    <>
+    <h1>hello my name is {g}</h1>
+    <h1>age={a/2}</h1>
+    </>
+    )
+}
+
+export default Product;
